9fce6b79a3be73f834c56e8891b1b73169d2689c
MatthewScott32 <matthewscottblagg@gmail.com>
2020-04-20 13:12:49 -0500

    zoo exampler

---
+++ movements/walking.py
@@ -0,0 +1,7 @@
+class IWalking:
+    def __init__(self):
+        self.walk = 0
+        self.legs = 0
+
+    def run(self):
+        print("The animal walks")
---
+++ zoo.py
@@ -0,0 +1,17 @@
+from animals import Penguin, PaintedDog
+from habitats import Aquarium
+
+bob = Penguin("Bob")
+bob.run()
+bob.swim()
+
+ralph = PaintedDog("Ralph")
+ralph.run()
+
+seaworld = Aquarium("Sea World")
+seaworld.add_swimmer_pythonic(bob)
+seaworld.add_swimmer_pythonic(ralph)
+seaworld.add_swimmer_type_check(ralph)
+
+for animal in seaworld.animals:
+    print(f'{animal} lives in Sea World.')
